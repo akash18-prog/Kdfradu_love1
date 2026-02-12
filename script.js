31017eced9dbf69289fa079b57b81aa00fd93fe8
akash18-prog <akashkohli1718@gmail.com>
2026-02-12 23:41:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 const song = document.getElementById("song");
 const loveSong = document.getElementById("loveSong");
+
 song.play();
 
 function unlock() {
-  const pass = document.getElementById("password").value;
-  if (pass === "kdfradu") {
-    document.getElementById("lockScreen").classList.remove("active");
-    document.getElementById("main").classList.add("active");
+  if (password.value === "kdfradu") {
+    lockScreen.classList.remove("active");
+    main.classList.add("active");
   } else {
-    document.getElementById("error").style.display = "block";
+    error.style.display = "block";
   }
 }
 
 function openGift(id) {
   document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
   document.getElementById(id).classList.add("active");
+
+  if (id === "gift2") {
+    song.pause();
+    loveSong.play();
+  } else {
+    loveSong.pause();
+    song.play();
+  }
 }
 
 function goBack() {
   document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
-  document.getElementById("main").classList.add("active");
+  main.classList.add("active");
+  loveSong.pause();
+  song.play();
 }
 
 const insta = document.getElementById("insta");
 insta.addEventListener("mouseover", () => {
-  insta.style.transform = `translate(${Math.random()*200-100}px,${Math.random()*200-100}px)`;
+  insta.style.transform =
+    `translate(${Math.random()*200-100}px,${Math.random()*200-100}px)`;
 });
 
 function chooseMe() {
-  song.pause();
-  loveSong.play();
   document.getElementById("gallery").style.display = "block";
 }
